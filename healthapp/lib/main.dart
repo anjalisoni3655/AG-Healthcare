bee87d2ff240c94698ec449e3c261cb17ef31b1e
anjalisoni3655 <anjalisony32@gmail.com>
2020-08-15 15:53:54 +0530

    chats

--- healthapp/lib/main.dart
+++ healthapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:healthapp/screens/chat.dart';
 import 'package:healthapp/screens/disease.dart';
 import 'package:healthapp/screens/doctor_list.dart';
 import 'package:healthapp/screens/login_screen.dart';
@@ -6,7 +7,7 @@ import 'package:healthapp/screens/home_screen.dart';
 import 'package:healthapp/screens/user_details.dart';
 import 'package:healthapp/screens/profile.dart';
 import 'package:healthapp/screens/index.dart';
-
+import 'package:healthapp/screens/chat.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -18,16 +19,18 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: LoginPage(),
+        home: LoginPage(title:'log in'),
         // initialRoute: LoginPage.id,
         routes: {
           IndexPage.id:(context)=>IndexPage(),
           UserForm.id: (context) => UserForm(),
           LoginPage.id: (context) => LoginPage(),
-          HomeScreen.id: (context) => HomeScreen(),
+        // HomeScreen.id: (context) => HomeScreen(),
            Profile.id: (context) => Profile(),
            Disease.id:(context)=>Disease(),
-           Doctor.id:(context)=>Doctor()
+           Doctor.id:(context)=>Doctor(),
+         //Chat.id:(context)=>Chat(),
+
 
         });
   }
