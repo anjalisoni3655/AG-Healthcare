ec7d9376cd84d46ed512417f24be5f9a5642ddb4
Imperial-lord <absatyaprakash01@gmail.com>
2020-09-02 07:12:03 +0530

    checking by pulling

--- healthapp/lib/screens/appointments/upcoming_page.dart
+++ healthapp/lib/screens/appointments/upcoming_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:healthapp/authentication/google_login.dart';
 import 'package:healthapp/authentication/user.dart' as globals;
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:healthapp/utils/settings.dart';
 import 'package:healthapp/widgets/loading.dart';
 
